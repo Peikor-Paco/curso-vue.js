98a5ddb4797a24b48e47d75d346cdf950431769f
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-09 17:45:38 +0100

    32. Desafío

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,4 +1,8 @@
-<!-- Ejemplo de reactividad -->
+<!-- Práctica
+Agrega un botón para disminuir el contador.
+Agrega un botón para resetear el contador.
+Pinta el contador en rojo cuando el valor sea menor a cero.
+Pinta el contador en verde cuando el valor sea mayor a cero. -->
 
 <!-- añadir setup a la etiqueta script -->
 <script setup> 
@@ -17,16 +21,41 @@ const increment = () => {
   counter.value ++;
 }
 
+const decrecent = () => {
+  counter.value --;
+}
+
+const reset = () => {
+  counter.value=0
+
+}
+
 
 </script>
 
 <!-- template puede renderizar todo el componente html -->
 <template>
+
   <!-- llamamos a lam fiunción que hemops declarado -->
   <h1>Hola {{name}}!</h1>
 
-  <h2>{{ counter }}</h2>
+  
+
+  <h2 v-if="counter <= 0" style="color:red">{{ counter }}</h2>
+  <h2 v-else style="color:green" >{{ counter }}</h2>
+
+
+ <!-- clases dinamicaS -->
+  <h2 :class="counter >= 0 ? 'positive' : 'negative'">
+        {{ counter }}
+  </h2>
+
   <button @click="increment">Aumentar</button>
+
+  <button @click="decrecent">Disminuir</button>
+
+  <button @click="reset">Reset</button>
+
 
 
 
@@ -36,4 +65,12 @@ const increment = () => {
   h1 {
     color: red;
   }
+
+  .negative {
+    color: red;
+}
+
+.positive {
+    color: green;
+}
 </style>
