d7bd6e5c9a3b687f627b73c584e72656e97444b8
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-09 13:39:15 +0100

    20. Uso de expresiones en Javascript

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -6,17 +6,34 @@
 const name = 'Vue dinámico';
 // v-bind permite comunicar le script y el template en los atributos
 const styleColor = "color: blue";
+
+//Expresiones de javascript
+// Array
+const arrayColores = ["blue","yellow"]
+
+//Booleano
+const activo = true;
 </script>
 
 <!-- template puede renderizar todo el componente html -->
 <template>
   <!-- llamamos el contenido de la constante {{name}} -->
   <h1>Hola {{name}}!</h1>
+
+  <h1>Hola {{name.toUpperCase()}}!</h1>
   <h2 style="color: blue">Tengo un color azul</h2>
   <!-- añadimos el v-bind: para llamar -->
   <h2 v-bind:style="styleColor">Tengo un color azul con v-bind variable</h2>
   <!-- podemos dejar solo los dos punto : -->
   <h2 :style="styleColor">Tengo un color azul con dos puntos variable</h2>
+
+
+  <h2>{{ arrayColores }}</h2>
+
+  <h2>{{ activo ? "Soy activo" : "No soy activo" }}</h2>
+
+
+  
 </template>
 
 <style>
