bc1af6591dd903435c5daa2b2f6d383abbc5ac79
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-09 14:11:51 +0100

    24. Recorriendo objetos con v-for

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -5,6 +5,9 @@
 //interpolación de texto
 const name = 'Vue dinámico';
 
+
+
+// array
 const arrayFrutas = [
         {
             name: "Manzana",
@@ -23,14 +26,20 @@ const arrayFrutas = [
         },
     ];
 
+// objeto
+const objetoFruta = {
+        name: "Naranja",
+        price: "$3.00",
+        description: "Una naranja",
+    };
+
 </script>
 
 <!-- template puede renderizar todo el componente html -->
 <template>
   <!-- llamamos el contenido de la constante {{name}} -->
   <h1>Hola {{name}}!</h1>
-
-  <!-- v-for -->
+  <!-- v-for Array -->
   <ul>
     <li v-for="(fruta) in arrayFrutas" :key="fruta.name">
       {{ fruta.name }} - 
@@ -39,7 +48,27 @@ const arrayFrutas = [
     </li>
   </ul>
 
-
+  <span>{{objetoFruta}}</span>
+  
+  <!-- v-for Objetos -->
+  <p>Valor</p>
+  <ul>
+      <li v-for="(value) in objetoFruta" :key="value">
+          {{ value }}
+      </li>
+  </ul>
+  <p>valor y priopiedad</p>
+  <ul>
+      <li v-for="(value, propiedad) in objetoFruta" :key="value">
+          {{ propiedad }} : {{ value }}
+      </li>
+  </ul>
+  <p>valor y priopiedad index</p>
+  <ul>
+      <li v-for="(value, propiedad, index) in objetoFruta" key="index">
+        {{ index + 1 }} - {{ propiedad }} : {{ value }}
+      </li>
+  </ul>
 </template>
 
 <style>
