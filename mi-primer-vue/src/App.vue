df66f062c8522e4e5aeabe2e70f8a1b05279d652
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-09 13:20:16 +0100

    17. SFC: single file components

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,3 +1,17 @@
+<!-- single file component logica, estructura y estilos en un solo fichero -->
+
+<!-- añadir setup a la etiqueta script -->
+<script setup> 
+
+</script>
+
+<!-- template puede renderizar todo el componente html -->
 <template>
   <h1>Hola Vue.js</h1>
 </template>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
