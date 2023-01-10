13e4738061b0d4ccbb8dfe820967d6276110d3f0
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-10 10:37:28 +0100

    45. Instalar Bootstrap vía NPM

---
+++ componentes-1/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+// importamos el componente boton contador
+import ButtonCounter from './components/ButtonCounter.vue';
+
+</script>
+
+<template>
+<div class="container ">
+  <div class="d-flex align-items-center flex-column">
+
+    <h1>APP</h1>
+    <br>
+    <div class="d-flex">
+      <!-- llamamos al bototn-->
+      <ButtonCounter></ButtonCounter>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ componentes-1/src/components/ButtonCounter.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { ref } from "vue";
+
+const counter = ref(0)
+
+const increment = () => {
+  counter.value ++;
+}
+
+
+</script>
+
+
+<template>
+  <button class="btn btn-success mx-1" @click="increment">Aumentar {{ counter }}</button>
+</template>
+
+<style scoped>
+
+</style>
